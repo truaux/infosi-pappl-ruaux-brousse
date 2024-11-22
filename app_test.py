a5472428d6c7ef6ef846dd983126d9946c67455c
Titouan Brousse <titouan.br@gmail.com>
2024-11-22 12:59:38 +0100

    nouveau code pour créer l'appli

---
+++ app_test.py
@@ -0,0 +1,46 @@
+import tkinter as tk
+from tkinter import messagebox
+
+def click():
+    name = entry.get()
+    hello.config(text="Bonjour " + name)
+    messagebox.showinfo("Hello", "Bonjour " + name)
+
+#def window
+window = tk.Tk()
+window.title("Notre app")
+window.geometry("400x400")
+
+#def label
+hello = tk.Label(window, text = "Hello World",
+bg="black",
+fg="green",
+font=("Arial", 20),
+width=20,
+height=2)
+hello.pack()
+
+#def frame
+frame = tk.Frame(window, width=400, height=5)
+frame.pack()
+
+#def entry
+entry = tk.Entry(window,
+width=25)
+entry.pack()
+
+#def frame
+frame = tk.Frame(window, width=400, height=5)
+frame.pack()
+
+#def button
+button = tk.Button(window,
+text="Entrer votre nom",
+width=25,
+height=2,
+bg="red",
+fg="white",
+command=click)
+button.pack()
+
+window.mainloop()
